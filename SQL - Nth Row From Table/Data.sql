ddbd2d2b29743bfb213432e9f5d22265d207dd8a
ShipOfLearning <kirankumar.roy@gmail.com>
2025-09-16 16:15:04 +0530

    Initial commit

---
+++ SQL - Nth Row From Table/Data.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Employees (
+    EmpId INT PRIMARY KEY,
+    EmpName VARCHAR(50),
+    Salary INT
+);
+
+INSERT INTO Employees (EmpId, EmpName, Salary) VALUES
+(1, 'Amit', 50000),
+(2, 'Neha', 70000),
+(3, 'Rajesh', 60000),
+(4, 'Priya', 90000),
+(5, 'Suresh', 80000),
+(6, 'Anita', 70000),
+(7, 'Ravi', 50000);
